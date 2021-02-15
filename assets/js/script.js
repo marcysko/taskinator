81700de4a8bb0a1a67417bcdef8475f0c4e244e7
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-02-15 11:36:29 -0700

    add submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-};
-  
- buttonEl.addEventListener("click" , createTaskHandler);
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
+
 
 
